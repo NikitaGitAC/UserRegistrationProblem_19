98a67f1f998d961196deddd06445ce5dc5c94383
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-08 15:45:07 +0530

    user input take and getting output

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Registration Page");
+            validfirstname name = new validfirstname();
+            Console.WriteLine("Enter the First Name:");
+            string FName = Console.ReadLine();
+            Console.WriteLine(name.ValidateFirstName(FName));
         }
     }
 }
