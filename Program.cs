ff976ef98383e3d954165bc38e321e862c720759
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-08 17:14:34 +0530

    Taking user input and printing output

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@
         {
             Console.WriteLine("Welcome to the User Registration Page");
             Console.WriteLine("Welcome to the User Registration Page");
-            validlastname name = new validlastname();
-            Console.WriteLine("Enter Last Name");
-            string LName = Console.ReadLine();
-            Console.WriteLine(name.ValidateLastName(LName));
+            validateemail eMail = new validateemail();
+            Console.WriteLine("Please enter the email:");
+            string email = Console.ReadLine();
+            Console.WriteLine(eMail.validateEmail(email));
         }
     }
 }
