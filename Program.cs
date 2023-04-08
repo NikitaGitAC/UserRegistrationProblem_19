7932ceda3f5e46bf13bcc86f8101f01785da4776
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-08 14:33:31 +0530

    greeting msg

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Registration Page");
+        }
+    }
+}
