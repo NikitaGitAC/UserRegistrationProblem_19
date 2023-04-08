cbf5f0c811fabfb1fa8aa7eacec7b48eb966cc60
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-08 15:53:00 +0530

    validating first name

---
+++ validfirstname.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+
+namespace UserRegistrationProblem
+{
+    public class validfirstname
+    {
+        public static string FirstName = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public bool ValidateFirstName(string FName)
+        {
+            return Regex.IsMatch(FName, FirstName);
+        }
+    }
+}
