e4fb0e5acd2a87fd5d11c7b8e457f440cd7ef8b0
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-17 21:08:56 +0530

    any 8 or more char for valid password and atleast 1 upper char

---
+++ passwordrule2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    public class passwordrule2
+    {
+        public static string PassWord = "^(?=.*[A-Z]).{8,}$";
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, PassWord);
+        }
+    }
+}
