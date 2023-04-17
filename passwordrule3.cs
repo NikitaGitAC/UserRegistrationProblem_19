ae0c3c3ecf2d439c259d4de65356df94d16403eb
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-17 21:23:49 +0530

    code to giving conditions to password to have atleast one digit

---
+++ passwordrule3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    public class passwordrule3
+    {
+        public static string PassWord = "^(?=.*[0-9])(?=.*[A-Z]).{8,}$";
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, PassWord);
+        }
+    }
+}
